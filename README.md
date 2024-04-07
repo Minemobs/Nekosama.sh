ee5c5dbe172c2383547dee612aa2515310947ddd
minemobs <issahakim94@gmail.com>
2024-04-07 14:43:54 +0200

    Replace gum with fzf More people already have fzf installed on their computer

--- README.md
+++ README.md
@@ -6,7 +6,7 @@ A simple downloader written in bash
 `./nekosama.sh "$(./nekosama-tui.sh)"` or `./nekosama.sh URL`
 
 ## Requirements:
- - gum (needed for `nekosama-tui.sh`)
+ - fzf (needed for `nekosama-tui.sh`)
  - jq
 
 https://github.com/Minemobs/Nekosama.sh/assets/38861340/61c97b63-feca-40c0-b958-5db8b05c4d05
--- nekosama-tui.sh
+++ nekosama-tui.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-if ! [ -x "$(command -v gum)" ]; then
-  echo 'Error: gum is not installed.' >&2
+if ! [ -x "$(command -v fzf)" ]; then
+  echo 'Error: fzf is not installed.' >&2
   exit 1
 fi
 if ! [ -x "$(command -v jq)" ]; then
@@ -17,17 +17,21 @@ function generateSequence {
       break
     fi
     if [[ $nbrOfEpisodes -lt 10 ]]; then
-      sequence="0$nbrOfEpisodes $sequence"
+      sequence="0$nbrOfEpisodes\n$sequence"
       continue
     fi
-    sequence="$nbrOfEpisodes $sequence"
+    sequence="$nbrOfEpisodes\n$sequence"
   done
-  echo "$sequence"
+  echo -e -n $sequence
+}
+
+function custom_fzf {
+  fzf --border-label-pos 5:top --color=label:italic:red "$@"
 }
 
 JSON="$(curl -s https://neko-sama.fr/animes-search-vostfr.json)"
 
-SERIE_NAME="$(echo "$JSON" | jq -r '.[] | .title' | gum filter)"
+SERIE_NAME="$(echo "$JSON" | jq -r '.[] | .title' | custom_fzf --border-label "VOSTFR Anime Selection" --height 50%)"
 if [ $? -ne 0 ]; then
   exit 0
 fi
@@ -35,7 +39,9 @@ SERIE_JSON="$(echo "$JSON" | jq 'map(select(.title == "'"$SERIE_NAME"'")) | .[]'
 SERIE_URL="$(echo "$SERIE_JSON" | jq -r '.url' | cut -d "/" -f 4 | cut -d '_' -f 1)"
 SERIE_EPISODES="$(curl https://neko-sama.fr"$(echo "$SERIE_JSON" | jq -r '.url')" | grep --only-matching -E "episodes = \[\{.+\]" | cut -d "=" -f 2 | jq .[-1].num)"
 if [[ $SERIE_EPISODES != "Film" ]]; then
-  EPISODE="$(gum choose --header "Episode of $SERIE_NAME" $(generateSequence "$SERIE_EPISODES"))"
+  # shellcheck disable=SC2046
+  # EPISODE="$(gum choose --header "Episode of $SERIE_NAME" $(generateSequence "$SERIE_EPISODES"))"
+  EPISODE=$(generateSequence "$SERIE_EPISODES" | custom_fzf --border-label "$SERIE_NAME Episode Selection" --height 25%)
   if [ $? -ne 0 ]; then
     exit 0
   fi
