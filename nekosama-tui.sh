03d5cf47e649d96b092c5c688ad380fcb026aecd
minemobs <38861340+Minemobs@users.noreply.github.com>
2024-06-12 20:43:28 +0200

    Fixed nekosama-tui with new layout

--- nekosama-tui.sh
+++ nekosama-tui.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
+
+LANG="vostfr" # Change it to vf
+
 if ! [ -x "$(command -v fzf)" ]; then
   echo 'Error: fzf is not installed.' >&2
   exit 1
@@ -31,11 +34,11 @@ function custom_fzf {
   fzf --no-sort --border-label-pos 5:top --color=label:italic:red "$@"
 }
 
-JSON="$(curl -s https://neko-sama.fr/animes-search-vostfr.json)"
-echo "$JSON" >| "/tmp/nekosama/vostfr.json"
+JSON="$(curl -s https://neko-sama.fr/animes-search-$LANG.json)"
+echo "$JSON" >| "/tmp/nekosama/$LANG.json"
 
 if [ -x "$(command -v kitty )" ]; then
-  SERIE_NAME="$(echo "$JSON" | jq -r '.[] | .title' | custom_fzf --border-label "VOSTFR Anime Selection" --height 50% --preview='kitty icat --clear --transfer-mode=memory --unicode-placeholder --stdin=no --place="${FZF_PREVIEW_COLUMNS}x${FZF_PREVIEW_LINES}@0x0" $(cat /tmp/nekosama/vostfr.json | jq -r ".[{n}] | .url_image")')"
+  SERIE_NAME="$(echo "$JSON" | jq -r '.[] | .title' | custom_fzf --border-label "VOSTFR Anime Selection" --height 50% --preview='kitty icat --clear --transfer-mode=memory --unicode-placeholder --stdin=no --place="${FZF_PREVIEW_COLUMNS}x${FZF_PREVIEW_LINES}@0x0" $(cat /tmp/nekosama/'$LANG'.json | jq -r ".[{n}] | .url_image")')"
 else
   SERIE_NAME="$(echo "$JSON" | jq -r '.[] | .title' | custom_fzf --border-label "VOSTFR Anime Selection" --height 50%)"
 fi
@@ -45,9 +48,10 @@ if [ $? -ne 0 ]; then
 fi
 
 SERIE_JSON="$(echo "$JSON" | jq 'map(select(.title == "'"$SERIE_NAME"'")) | .[]')"
-SERIE_URL="$(echo "$SERIE_JSON" | jq -r '.url' | cut -d "/" -f 4 | cut -d '_' -f 1)"
-SERIE_EPISODES="$(curl https://neko-sama.fr"$(echo "$SERIE_JSON" | jq -r '.url')" | grep --only-matching -E "episodes = \[\{.+\]" | cut -d "=" -f 2 | jq .[-1].num)"
-if [[ $SERIE_EPISODES != "Film" ]]; then
+SERIE_URL="$(echo $SERIE_JSON | jq -r '.url')"
+SERIE_PATH="$(echo "$SERIE_URL" | cut -d "/" -f 6 | cut -d '_' -f 1)"
+SERIE_EPISODES="$(curl -s $SERIE_URL | grep -Eo "https://neko-sama.fr/anime/episode/[0-9]+-.+-[0-9]+_vostfr" | head -1 | grep -Eo "\-[0-9]+_" | cut -d "_" -f 1 | cut -d "-" -f 2)"
+if [[ $SERIE_EPISODES != "01" ]]; then
   # shellcheck disable=SC2046
   # EPISODE="$(gum choose --header "Episode of $SERIE_NAME" $(generateSequence "$SERIE_EPISODES"))"
   EPISODE=$(generateSequence "$SERIE_EPISODES" | custom_fzf --border-label "$SERIE_NAME Episode Selection" --height 25%)
@@ -58,5 +62,5 @@ else
   EPISODE="01"
 fi
 
-rm "/tmp/nekosama/vostfr.json" &> /dev/null
-printf "https://neko-sama.fr/anime/episode/%s-%s_vostfr\n" "$SERIE_URL" "$EPISODE"
+rm "/tmp/nekosama/$LANG.json" &> /dev/null
+printf "https://neko-sama.fr/anime/episode/%s-%s_vostfr\n" "$SERIE_PATH" "$EPISODE"
